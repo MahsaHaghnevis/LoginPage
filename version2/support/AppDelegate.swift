7708b7e1bf356f8b0cf2cdf7e6ead30d32afaf7d
Mahsa <haghnevismahsa@gmail.com>
2024-08-19 00:16:57 +0330

    cnaged the distance of textfield and keyboard to 40

--- version2/support/AppDelegate.swift
+++ version2/support/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.keyboardDistanceFromTextField = 40
         return true
     }
 
